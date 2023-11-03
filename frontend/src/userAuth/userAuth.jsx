5aacd9433bd202ee1ad14738c44d9bff5eeab018
Paulinus Ayere Aghughu <paulynpen@gmail.com>
2023-11-03 14:08:00 +0100

    updated the files

--- frontend/src/userAuth/userAuth.jsx
+++
@@ -1,7 +0,0 @@
-export const useGetUser = () => {
-  const token = window.localStorage.getItem("accessToken"); // Retrieve the user's access token
-  const userID = window.localStorage.getItem("userID"); // Retrieve the user's ID
-
-  // You can return the token and user ID or use them for making authenticated requests
-  return { token, userID };
-};
